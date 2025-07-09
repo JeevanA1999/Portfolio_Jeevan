ee086662e81e46330370fa54d0439915e3215bb5
JEEVAN A <70322715+JeevanA1999@users.noreply.github.com>
2025-07-09 23:43:55 +0530

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -11,10 +11,11 @@ import { FiFileText, FiExternalLink } from "react-icons/fi";
 
 import Work1 from "./assets/hotelBooking.png";
 import Work2 from "./assets/jobline.png";
-import Work3 from "./assets/gfinder.png";
-import Work4 from "./assets/cointrace.png";
-import Work5 from "./assets/newsNexus.png"
-import Work6 from "./assets/DashBoard.png"
+import Work3 from "./assets/adminDashboard.png";
+import Work4 from "./assets/gfinder.png";
+import Work5 from "./assets/cointrace.png";
+import Work6 from "./assets/newsNexus.png";
+import Work7 from "./assets/DashBoard.png";
 
 import Theme1 from "./assets/purple.png";
 import Theme2 from "./assets/red.png";
@@ -34,21 +35,18 @@ export const links = [
     icon: <FaHome className="nav__icon" />,
     path: "/",
   },
-
   {
     id: 2,
     name: "About",
     icon: <FaUser className="nav__icon" />,
     path: "/about",
   },
-
   {
     id: 3,
     name: "Portfolio",
     icon: <FaFolderOpen className="nav__icon" />,
     path: "/portfolio",
   },
-
   {
     id: 4,
     name: "Contact",
@@ -58,91 +56,23 @@ export const links = [
 ];
 
 export const personalInfo = [
-  {
-    id: 1,
-    title: "First Name : ",
-    description: "Jeevan",
-  },
-
-  {
-    id: 2,
-    title: "Last Name : ",
-    description: "A",
-  },
-
-  {
-    id: 3,
-    title: "Age : ",
-    description: "24 Years",
-  },
-
-  {
-    id: 4,
-    title: "Nationality : ",
-    description: "Indian",
-  },
-
-  {
-    id: 5,
-    title: "Freelance : ",
-    description: "Available",
-  },
-
-  {
-    id: 6,
-    title: "Address : ",
-    description: "Bangalore",
-  },
-
-  {
-    id: 7,
-    title: "Phone : ",
-    description: "+91 8618415611",
-  },
-
-  {
-    id: 8,
-    title: "Email : ",
-    description: "jeevukulal96@mail.com",
-  },
-
-  {
-    id: 9,
-    title: "GitHub : ",
-    description: "JeevanA1999",
-  },
-
-  {
-    id: 10,
-    title: "Language : ",
-    description: "English, Kannada, Hindi",
-  },
+  { id: 1, title: "First Name : ", description: "Jeevan" },
+  { id: 2, title: "Last Name : ", description: "A" },
+  { id: 3, title: "Age : ", description: "24 Years" },
+  { id: 4, title: "Nationality : ", description: "Indian" },
+  { id: 5, title: "Freelance : ", description: "Available" },
+  { id: 6, title: "Address : ", description: "Bangalore" },
+  { id: 7, title: "Phone : ", description: "+91 8618415611" },
+  { id: 8, title: "Email : ", description: "jeevukulal96@mail.com" },
+  { id: 9, title: "GitHub : ", description: "JeevanA1999" },
+  { id: 10, title: "Language : ", description: "English, Kannada, Hindi" },
 ];
 
 export const stats = [
-  {
-    id: 1,
-    no: "3",
-    title: "Years of <br /> Experience",
-  },
-
-  {
-    id: 2,
-    no: "10+",
-    title: "Completed <br /> Projects",
-  },
-
-  {
-    id: 3,
-    no: "5+",
-    title: "Happy <br /> Customers",
-  },
-
-  {
-    id: 4,
-    no: "3",
-    title: " Awards <br /> Won",
-  },
+  { id: 1, no: "3", title: "Years of <br /> Experience" },
+  { id: 2, no: "10+", title: "Completed <br /> Projects" },
+  { id: 3, no: "5+", title: "Happy <br /> Customers" },
+  { id: 4, no: "3", title: "Awards <br /> Won" },
 ];
 
 export const resume = [
@@ -152,120 +82,56 @@ export const resume = [
     icon: <FaBriefcase />,
     year: "2022 - PRESENT",
     title: "Web Developer <span> Wipro Ltd </span>",
-    desc: "Took the project from initial design through to completion, ensuring all milestones were met on time.Improved UI performance by 60% by identifying bottlenecks, leveraging technologies like Redux and ReactHooks.Integrated responsive design principles and turned static web pages into dynamic ones, reducing device-specific bugs by 35%.Coordinated with the design team to ensure seamless integration of UX/UI designs, resulting in a 60% increase in user satisfaction.Worked closely with an Agile team and provided end-to-end solutions for clients. Diagnosed and resolved issues during the development process, ensuring a stable and efficient final product",
+    desc: "Took the project from initial design through to completion, ensuring all milestones were met on time. Improved UI performance by 60% by identifying bottlenecks, leveraging technologies like Redux and React Hooks. Integrated responsive design principles and turned static web pages into dynamic ones, reducing device-specific bugs by 35%. Coordinated with the design team to ensure seamless integration of UX/UI designs, resulting in a 60% increase in user satisfaction. Worked closely with an Agile team and provided end-to-end solutions for clients.",
   },
-
   {
     id: 2,
     category: "experience",
     icon: <FaBriefcase />,
-    year: "2020 - 2020",
-    title: " Software Developer Intern <span> SmartBridge Educational Service Private Limited </span>",
-    desc: " Developed a mobile application using MIT App Inventor to monitor and control greenhouse conditions remotely. Implemented data archiving to IBM Cloud to ensure data availability for future analysis. Built an automated irrigation system using Arduino and Node-RED to optimize water usage in agriculture. Integrated sensors to monitor soil moisture levels and control water flow",
+    year: "2020",
+    title: "Software Developer Intern <span> SmartBridge Educational Services </span>",
+    desc: "Developed a mobile application using MIT App Inventor to monitor and control greenhouse conditions remotely. Implemented data archiving to IBM Cloud. Built an automated irrigation system using Arduino and Node-RED to optimize water usage.",
   },
-
-  // {
-  //   id: 3,
-  //   category: "experience",
-  //   icon: <FaBriefcase />,
-  //   year: "2005 - 2013",
-  //   title: "Consultant <span> Videohive </span>",
-  //   desc: "Lorem ipsum dolor sit amet, tempor incididunt ut laboreconsectetur elit, sed do eiusmod tempor duntt",
-  // },
-
   {
     id: 4,
     category: "education",
     icon: <FaGraduationCap />,
     year: "2017-2021",
-    title: "Engineering <span> Srinivas Institute of Technology Mangalore </span>",
+    title: "Engineering <span> Srinivas Institute of Technology, Mangalore </span>",
     desc: "Bachelor of Engineering in Electronics and Communication",
   },
-
   {
     id: 5,
     category: "education",
     icon: <FaGraduationCap />,
     year: "2015-2017",
-    title: "Pre-University  <span> Carmel Composite PU College, Bantwal  </span>",
-    desc: " Pre-University in PCMC",
+    title: "Pre-University <span> Carmel Composite PU College, Bantwal </span>",
+    desc: "Pre-University in PCMC",
   },
-
-  // {
-  //   id: 6,
-  //   category: "education",
-  //   icon: <FaGraduationCap />,
-  //   year: "2009",
-  //   title: "Bachelor Degree <span> Tunis High School </span>",
-  //   desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ut labore",
-  // },
 ];
 
 export const skills = [
-  {
-    id: 1,
-    title: "Html",
-    percentage: "80",
-  },
-
-  {
-    id: 2,
-    title: "Javascript",
-    percentage: "70",
-  },
-
-  {
-    id: 3,
-    title: "Css",
-    percentage: "80",
-  },
-
-  {
-    id: 4,
-    title: "React",
-    percentage: "70",
-  },
-
-  {
-    id: 5,
-    title: "Tailwind Css",
-    percentage: "60",
-  },
-  {
-    id: 6,
-    title: "Bootstrap",
-    percentage: "45",
-  },
-  {
-    id: 7,
-    title: "Redux",
-    percentage: "62",
-  },
-  {
-    id: 8,
-    title: "Git",
-    percentage: "73",
-  },
-
-
+  { id: 1, title: "HTML", percentage: "80" },
+  { id: 2, title: "JavaScript", percentage: "70" },
+  { id: 3, title: "CSS", percentage: "80" },
+  { id: 4, title: "React", percentage: "70" },
+  { id: 5, title: "Tailwind CSS", percentage: "60" },
+  { id: 6, title: "Bootstrap", percentage: "45" },
+  { id: 7, title: "Redux", percentage: "62" },
+  { id: 8, title: "Git", percentage: "73" },
+  { id: 9, title: "Next.js", percentage: "60" },
+  { id: 10, title: "Material UI", percentage: "77" },
 ];
+
 export const portfolio = [
   {
-    id: 6,
-    img: Work6,
-    title: "Transcation Dash Board",
-    link:"https://transcation-control-dash-board.vercel.app/",
+    id: 7,
+    img: Work7,
+    title: "Transaction Dashboard",
+    link: "https://transcation-control-dash-board.vercel.app/",
     details: [
-      {
-        icon: <FiFileText />,
-        title: "Project : ",
-        desc: "Transcation Dash Board",
-      },
-      {
-        icon: <FaCode />,
-        title: "Language: ",
-        desc: "HTML5, CSS3, ReactJs, Redux Toolkit",
-      },
+      { icon: <FiFileText />, title: "Project : ", desc: "Transaction Dashboard" },
+      { icon: <FaCode />, title: "Language: ", desc: "HTML5, CSS3, ReactJs, Redux Toolkit" },
       {
         icon: <FiExternalLink />,
         title: "Preview : ",
@@ -282,23 +148,14 @@ export const portfolio = [
       },
     ],
   },
-
   {
-    id: 4,
-    img: Work5,
+    id: 6,
+    img: Work6,
     title: "News Nexus",
-    link:"https://github.com/JeevanA1999/NewsNexusOne/",
+    link: "https://github.com/JeevanA1999/NewsNexusOne/",
     details: [
-      {
-        icon: <FiFileText />,
-        title: "Project : ",
-        desc: "News Nexus",
-      },
-      {
-        icon: <FaCode />,
-        title: "Language: ",
-        desc: "React Js, Redux Toolkit,Tailwind CSS",
-      },
+      { icon: <FiFileText />, title: "Project : ", desc: "News Nexus" },
+      { icon: <FaCode />, title: "Language: ", desc: "React Js, Redux Toolkit, Tailwind CSS" },
       {
         icon: <FiExternalLink />,
         title: "Preview : ",
@@ -319,18 +176,10 @@ export const portfolio = [
     id: 1,
     img: Work1,
     title: "Hotel Room Booking",
-    link:"https://hotel-booking-sooty.vercel.app/",
+    link: "https://hotel-booking-sooty.vercel.app/",
     details: [
-      {
-        icon: <FiFileText />,
-        title: "Project : ",
-        desc: "Hotel Satkar",
-      },
-      {
-        icon: <FaCode />,
-        title: "Language: ",
-        desc: "HTML5, CSS3, ReactJs",
-      },
+      { icon: <FiFileText />, title: "Project : ", desc: "Hotel Satkar" },
+      { icon: <FaCode />, title: "Language: ", desc: "HTML5, CSS3, ReactJs" },
       {
         icon: <FiExternalLink />,
         title: "Preview : ",
@@ -347,23 +196,14 @@ export const portfolio = [
       },
     ],
   },
-
   {
     id: 2,
     img: Work2,
     title: "Job Finder Application",
-    link:"https://job-finder-six.vercel.app/",
+    link: "https://job-finder-six.vercel.app/",
     details: [
-      {
-        icon: <FiFileText />,
-        title: "Project: ",
-        desc: "Job Line",
-      },
-      {
-        icon: <FaCode />,
-        title: "Language : ",
-        desc: "React JS",
-      },
+      { icon: <FiFileText />, title: "Project: ", desc: "Job Line" },
+      { icon: <FaCode />, title: "Language : ", desc: "React JS" },
       {
         icon: <FiExternalLink />,
         title: "Preview : ",
@@ -380,23 +220,38 @@ export const portfolio = [
       },
     ],
   },
-
   {
     id: 3,
     img: Work3,
-    title: "Github Finder",
-    link:"https://jeevana1999.github.io/Github_Profile_Finder/",
+    title: "Admin Dashboard",
+    link: "https://vitejs-vite-uayajych.vercel.app/",
     details: [
+      { icon: <FiFileText />, title: "Project: ", desc: "Admin Dashboard" },
+      { icon: <FaCode />, title: "Language : ", desc: "React Js" },
       {
-        icon: <FiFileText />,
-        title: "Project: ",
-        desc: "G Finder",
+        icon: <FiExternalLink />,
+        title: "Preview : ",
+        desc: (
+          <a
+            className="item__details_Link"
+            href="https://vitejs-vite-uayajych.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Website
+          </a>
+        ),
       },
-      {
-        icon: <FaCode />,
-        title: "Language : ",
-        desc: "React Js",
-      },
+    ],
+  },
+  {
+    id: 4,
+    img: Work4,
+    title: "GitHub Finder",
+    link: "https://jeevana1999.github.io/Github_Profile_Finder/",
+    details: [
+      { icon: <FiFileText />, title: "Project: ", desc: "G Finder" },
+      { icon: <FaCode />, title: "Language : ", desc: "React Js" },
       {
         icon: <FiExternalLink />,
         title: "Preview : ",
@@ -413,23 +268,14 @@ export const portfolio = [
       },
     ],
   },
-
   {
-    id: 4,
-    img: Work4,
+    id: 5,
+    img: Work5,
     title: "Coin Trace",
-    link:"https://coin-trace-h6my.vercel.app/",
+    link: "https://coin-trace-h6my.vercel.app/",
     details: [
-      {
-        icon: <FiFileText />,
-        title: "Project: ",
-        desc: "Coin Trace",
-      },
-      {
-        icon: <FaCode />,
-        title: "Language : ",
-        desc: "React Js",
-      },
+      { icon: <FiFileText />, title: "Project: ", desc: "Coin Trace" },
+      { icon: <FaCode />, title: "Language : ", desc: "React Js" },
       {
         icon: <FiExternalLink />,
         title: "Preview : ",
@@ -447,64 +293,16 @@ export const portfolio = [
     ],
   },
 ];
+
 export const themes = [
-  {
-    id: 1,
-    img: Theme1,
-    color: "hsl(252, 35%, 51%)",
-  },
-
-  {
-    id: 2,
-    img: Theme2,
-    color: "hsl(4, 93%, 54%)",
-  },
-
-  {
-    id: 3,
-    img: Theme3,
-    color: "hsl(271, 76%, 53%)",
-  },
-
-  {
-    id: 4,
-    img: Theme4,
-    color: "hsl(225, 73%, 57%)",
-  },
-
-  {
-    id: 5,
-    img: Theme5,
-    color: "hsl(43, 74%, 49%)",
-  },
-
-  {
-    id: 6,
-    img: Theme6,
-    color: "hsl(339, 81%, 66%)",
-  },
-
-  {
-    id: 7,
-    img: Theme7,
-    color: "hsl(80, 61%, 50%)",
-  },
-
-  {
-    id: 8,
-    img: Theme8,
-    color: "hsl(19, 96%, 52%)",
-  },
-
-  {
-    id: 9,
-    img: Theme9,
-    color: "hsl(88, 65%, 43%)",
-  },
-
-  {
-    id: 10,
-    img: Theme10,
-    color: "hsl(42, 100%, 50%)",
-  },
+  { id: 1, img: Theme1, color: "hsl(252, 35%, 51%)" },
+  { id: 2, img: Theme2, color: "hsl(4, 93%, 54%)" },
+  { id: 3, img: Theme3, color: "hsl(271, 76%, 53%)" },
+  { id: 4, img: Theme4, color: "hsl(225, 73%, 57%)" },
+  { id: 5, img: Theme5, color: "hsl(43, 74%, 49%)" },
+  { id: 6, img: Theme6, color: "hsl(339, 81%, 66%)" },
+  { id: 7, img: Theme7, color: "hsl(80, 61%, 50%)" },
+  { id: 8, img: Theme8, color: "hsl(19, 96%, 52%)" },
+  { id: 9, img: Theme9, color: "hsl(88, 65%, 43%)" },
+  { id: 10, img: Theme10, color: "hsl(42, 100%, 50%)" },
 ];
